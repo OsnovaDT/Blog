79125edbbcd2be33f6f67f775d03024eb70f8370
Evgeny Stepanov <maestrojane@gmail.com>
2022-01-15 12:59:16 +0300

    Add admin for Post model

--- post/admin.py
+++ post/admin.py
@@ -1,1 +1,15 @@
 """Registered models"""
+
+from django.contrib import admin
+from post.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Admin panel for Post model"""
+
+    list_display = (
+        'title', 'author', 'status',
+        'published_time', 'updated_time',
+    )
+    list_filter = ('published_time', 'status',)
