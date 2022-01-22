fa10665df87fdba2bf95519ce850ddaf527ecbc5
Evgeny Stepanov <maestrojane@gmail.com>
2022-01-22 23:41:39 +0300

    Add password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,9 @@
 
 from django.urls import path, reverse_lazy
 from django.contrib.auth.views import (
-    LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
+    LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView,
+    PasswordResetCompleteView, PasswordResetConfirmView,
+    PasswordResetDoneView, PasswordResetView
 )
 
 from accounts.views import SignUp
@@ -25,5 +27,45 @@ urlpatterns = [
             success_url=reverse_lazy('post:all_posts'),
         ),
         name='password_change'
+    ),
+
+    path(
+        'password_reset/',
+        PasswordResetView.as_view(
+            template_name='accounts/password_reset/password_reset.html',
+            subject_template_name=\
+                'accounts/password_reset/message_for_user_subject.txt',
+            email_template_name=\
+                'accounts/password_reset/message_for_user_body.html',
+            success_url=reverse_lazy('accounts:message_for_user_sent'),
+        ),
+        name="password_reset"
+    ),
+
+    path(
+        'password_reset/message_sent/',
+        PasswordResetDoneView.as_view(
+            template_name='accounts/password_reset/message_for_user_sent.html',
+            extra_context={'is_message_sent': True},
+        ),
+        name="message_for_user_sent"
+    ),
+
+    path(
+        'password_reset/<uidb64>/<token>/',
+        PasswordResetConfirmView.as_view(
+            template_name='accounts/password_reset/set_password.html',
+            success_url=reverse_lazy('accounts:password_successfully_reset'),
+        ),
+        name="set_password"
+    ),
+
+    path(
+        'password_reset/done/',
+        PasswordResetCompleteView.as_view(
+            template_name=\
+                'accounts/password_reset/password_successfully_reset.html',
+        ),
+        name='password_successfully_reset'
     ),
 ]
