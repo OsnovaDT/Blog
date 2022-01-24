9b1bf115912c1a2c9bf5678b994261cc634bffb0
OsnovaDT <maestorjane@gmail.com>
2022-01-24 21:38:47 +0300

    Add user authentication by JWT from console

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,9 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, TokenRefreshView, TokenVerifyView
+)
 
 
 urlpatterns = [
@@ -11,10 +14,26 @@ urlpatterns = [
     path('post/', include('post.urls')),
     path('accounts/', include('accounts.urls')),
     path('user/', include('user.urls')),
-
     path(
         'api-auth/',
         include('rest_framework.urls', namespace='rest_framework')
+    ),
+
+    # API token
+    path(
+        'api/token/',
+        TokenObtainPairView.as_view(),
+        name='token_obtain_pair'
+    ),
+    path(
+        'api/token/refresh/',
+        TokenRefreshView.as_view(),
+        name='token_refresh'
+    ),
+    path(
+        'api/token/verify/',
+        TokenVerifyView.as_view(),
+        name='token_verify'
     ),
 ]
 
