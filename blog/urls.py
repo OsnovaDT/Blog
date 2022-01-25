e91d50e5f44c5f7986fa77e1cc49f99b1d3c4a74
Evgeny Stepanov <maestrojane@gmail.com>
2022-01-25 20:05:05 +0300

    Refactoring (blog folder)

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,8 @@
 """Routes of the project"""
 
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView, TokenRefreshView, TokenVerifyView
@@ -10,39 +10,50 @@ from rest_framework_simplejwt.views import (
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('post/', include('post.urls')),
-    path('accounts/', include('accounts.urls')),
-    path('user/', include('user.urls')),
+    path('admin/', admin.site.urls,),
+
+    # Custom apps
+    path('post/', include('post.urls'),),
+
+    path('accounts/', include('accounts.urls'),),
+
+    path('user/', include('user.urls'),),
+
+    # DRF
     path(
         'api-auth/',
-        include('rest_framework.urls', namespace='rest_framework')
+        include(
+            'rest_framework.urls',
+            namespace='rest_framework',
+        ),
     ),
 
     # API token
     path(
         'api/token/',
         TokenObtainPairView.as_view(),
-        name='token_obtain_pair'
+        name='token_obtain_pair',
     ),
+
     path(
         'api/token/refresh/',
         TokenRefreshView.as_view(),
-        name='token_refresh'
+        name='token_refresh',
     ),
+
     path(
         'api/token/verify/',
         TokenVerifyView.as_view(),
-        name='token_verify'
+        name='token_verify',
     ),
 ]
 
 urlpatterns += static(
     settings.IMAGES_RELATIVE_PATH,
-    document_root=settings.IMAGES_ROOT
+    document_root=settings.IMAGES_ROOT,
 )
 
 urlpatterns += static(
     settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
+    document_root=settings.MEDIA_ROOT,
 )
