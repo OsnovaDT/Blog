2e2759fb65de4d3e0282347cce85b7f60095a409
Evgeny Stepanov <maestrojane@gmail.com>
2022-01-27 22:40:25 +0300

    (Bot) Refactoring

--- bot/main.py
+++ bot/main.py
@@ -1,44 +1,44 @@
 """Bot on Selenium
 
-The bot get variables from .env file.
-Then it performs these actions:
+Bot's actions:
     1) Register users
     2) Creating a random number of posts
     3) Like a random number of posts
+    4) Dislike a random number of posts
 
 """
 
 from random import randint
 
-from decouple import config
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+from constants import *
 
-USERS_AMOUNT = int(config('USERS_AMOUNT'))
 
-MAX_POSTS_PER_USER = int(config('MAX_POSTS_PER_USER'))
+def get_firefox_driver() -> webdriver.Firefox:
+    """Return customized Firefox driver"""
 
-MAX_LIKES_PER_USER = int(config('MAX_LIKES_PER_USER'))
+    firefox_driver_capabilities = DesiredCapabilities().FIREFOX
+    firefox_driver_capabilities["marionette"] = True
 
-GECKODRIVER_PATH = config('GECKODRIVER_PATH')
+    firefox_options = Options()
+    firefox_options.headless = True
 
-SITE_DOMAIN = 'http://127.0.0.1:8000/'
+    firefox_driver = webdriver.Firefox(
+        capabilities=firefox_driver_capabilities,
+        executable_path=GECKODRIVER_PATH,
+        options=firefox_options,
+    )
 
-USER_PASSWORD = 'OsnovaDTBLog'
-
-LIKE_CLASS = 'post_like'
-
-DISLIKE_CLASS = 'post_dislike'
-
-POST_ESTIMATION_CLASS = 'post_estimation'
+    return firefox_driver
 
 
 def click_submit_button(driver: webdriver) -> None:
     """Click submit button on the form"""
 
-    submit_button = driver.find_element_by_class_name("submit_button")
+    submit_button = driver.find_element_by_class_name(SUBMIT_BUTTON_CLASS)
     submit_button.click()
 
 
@@ -50,9 +50,9 @@ def fill_input(name: str, value: str, driver: webdriver) -> None:
 
 
 def register_user(username: str, password: str, driver: webdriver) -> None:
-    """Signup user on the site"""
+    """Signup the user on the site"""
 
-    driver.get(SITE_DOMAIN + "accounts/signup/")
+    driver.get(SITE_DOMAIN + SIGNUP_URL)
 
     fill_input("username", username, driver)
     fill_input("password1", password, driver)
@@ -64,9 +64,9 @@ def register_user(username: str, password: str, driver: webdriver) -> None:
 
 
 def authenticate_user(username: str, password: str, driver: webdriver) -> None:
-    """Authenticate user on the site"""
+    """Authenticate the user on the site"""
 
-    driver.get(SITE_DOMAIN + "accounts/login/")
+    driver.get(SITE_DOMAIN + LOGIN_URL)
 
     fill_input("username", username, driver)
     fill_input("password", password, driver)
@@ -79,7 +79,7 @@ def authenticate_user(username: str, password: str, driver: webdriver) -> None:
 def create_post(title: str, content: str, driver: webdriver) -> None:
     """Create user's post"""
 
-    driver.get(SITE_DOMAIN + "post/create/")
+    driver.get(SITE_DOMAIN + POST_CREATE_URL)
 
     fill_input("title", title, driver)
     fill_input("content", content, driver)
@@ -89,23 +89,23 @@ def create_post(title: str, content: str, driver: webdriver) -> None:
     print(f'Created post "{title}"')
 
 
-def create_user_posts(username: str, posts_amount: int, driver: webdriver):
-    """Create posts for the user"""
+def create_all_user_posts(username: str, posts_amount: int, driver: webdriver):
+    """Create all posts for the user"""
 
-    for post_number in range(posts_amount):
+    for post_number in range(1, posts_amount + 1):
         create_post(
-            f'Title for post {post_number + 1} created by {username}',
-            f'Content for post {post_number + 1} created by {username}',
+            f'Title for post {post_number} created by {username}',
+            f'Content for post {post_number} created by {username}',
             driver,
         )
 
 
 def get_all_posts(driver: webdriver) -> list:
-    """Return all divs with post_estimation class from post/all/ page"""
+    """Return all divs with post_estimation class from posts page"""
 
-    driver.get(SITE_DOMAIN + "post/all/")
+    driver.get(SITE_DOMAIN + ALL_POSTS_URL)
 
-    return driver.find_elements_by_class_name(POST_ESTIMATION_CLASS)
+    return driver.find_elements_by_class_name(ESTIMATION_CLASS)
 
 
 def like_post(post: webdriver) -> None:
@@ -120,21 +120,38 @@ def dislike_post(post: webdriver) -> None:
     post.find_element_by_class_name(DISLIKE_CLASS).click()
 
 
+def get_random_bool() -> bool:
+    """Randomly return True or False"""
+
+    return bool(randint(0, 1))
+
+
+def like_posts_by_one_user(driver: webdriver):
+    """Randomly like posts by one user"""
+
+    user_likes = 0
+
+    all_posts = get_all_posts(driver)
+
+    for post in all_posts:
+        if user_likes < MAX_LIKES_PER_USER:
+            is_user_likes_post = get_random_bool()
+
+            if is_user_likes_post:
+                like_post(post)
+                user_likes += 1
+            else:
+                is_user_dislikes_post = get_random_bool()
+
+                if is_user_dislikes_post:
+                    dislike_post(post)
+
+
 if __name__ == '__main__':
-    firefox_driver_capabilities = DesiredCapabilities().FIREFOX
-    firefox_driver_capabilities["marionette"] = True
-
-    firefox_options = Options()
-    firefox_options.headless = True
-
-    firefox_driver = webdriver.Firefox(
-        capabilities=firefox_driver_capabilities,
-        executable_path=GECKODRIVER_PATH,
-        options=firefox_options,
-    )
+    firefox_driver = get_firefox_driver()
 
     for user_number in range(USERS_AMOUNT):
-        username = f'selenium_test_{user_number + 1}'
+        username = f'selenium_test_user_{user_number + 1}'
         posts_amount = randint(1, MAX_POSTS_PER_USER)
 
         register_user(
@@ -145,31 +162,17 @@ if __name__ == '__main__':
             username, USER_PASSWORD, firefox_driver,
         )
 
-        create_user_posts(
+        create_all_user_posts(
             username, posts_amount, firefox_driver,
         )
 
     for user_number in range(USERS_AMOUNT):
-        user_likes = 0
-        username = f'selenium_test_{user_number + 1}'
+        username = f'selenium_test_user_{user_number + 1}'
 
         authenticate_user(
             username, USER_PASSWORD, firefox_driver,
         )
 
-        all_posts = get_all_posts(firefox_driver)
-
-        for post in all_posts:
-            if user_likes < MAX_LIKES_PER_USER:
-                is_user_likes_post = bool(randint(0, 1))
-
-                if is_user_likes_post:
-                    like_post(post)
-                    user_likes += 1
-                else:
-                    is_user_dislikes_post = bool(randint(0, 1))
-
-                    if is_user_dislikes_post:
-                        dislike_post(post)
+        like_posts_by_one_user(firefox_driver)
 
     firefox_driver.close()
