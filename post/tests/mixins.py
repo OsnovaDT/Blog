c4e76e9ae5812a9e668e28a0f35bbe8a26cc6e1e
OsnovaDT <maestrojane@gmail.com>
2022-01-29 23:23:38 +0300

    Add tests for Post model

--- post/tests/mixins.py
+++ post/tests/mixins.py
@@ -42,6 +42,90 @@ class TestAutoNowAddMixin:
                 self.assertTrue(auto_now_add_value)
 
 
+class TestMaxLengthMixin:
+    """Mixin with function for testing max_length parameter"""
+
+    def test_max_length(self, model):
+        """Test max_length parameter for fields"""
+
+        test_parameter_of_field(
+            model, self, self.field_and_max_length, 'max_length'
+        )
+
+
+class TestDbIndexMixin:
+    """Mixin with function for testing db_index parameter"""
+
+    def test_db_index(self, model):
+        """Test db_index parameter for fields"""
+
+        for object_ in model.objects.all():
+            for field in self.db_index_fields:
+                db_index_value = object_._meta.get_field(field).db_index
+
+                self.assertTrue(db_index_value)
+
+
+class TestAutoNowMixin:
+    """Mixin with function for testing auto_now parameter"""
+
+    def test_auto_now(self, model):
+        """Test auto_now parameter for fields"""
+
+        for object_ in model.objects.all():
+            for field in self.auto_now_fields:
+                auto_now_value = object_._meta.get_field(field).auto_now
+
+                self.assertEqual(auto_now_value, True)
+
+
+class TestChoicesMixin:
+    """Mixin with function for testing choices parameter"""
+
+    def test_choices(self, model):
+        """Test choices parameter for fields"""
+
+        test_parameter_of_field(
+            model, self, self.field_and_choices, 'choices'
+        )
+
+
+class TestBlankMixin:
+    """Mixin with function for testing blank parameter"""
+
+    def test_blank(self, model):
+        """Test blank parameter for fields"""
+
+        for instance in model.objects.all():
+            for field in self.blank_fields:
+                real_blank = instance._meta.get_field(field).blank
+
+                self.assertTrue(real_blank)
+
+
+class TestDefaultMixin:
+    """Mixin with function for testing default parameter"""
+
+    def test_default(self, model):
+        """Test default parameter for fields"""
+
+        test_parameter_of_field(
+            model, self, self.field_and_default, 'default'
+        )
+
+
+class TestModelOrdering:
+    """Mixin with function for testing ordering of model"""
+
+    def test_ordering_of_model(self, model):
+        """Test ordering of the model"""
+
+        real_ordering = model._meta.ordering
+        expected_ordering = self.model_ordering
+
+        self.assertEqual(real_ordering, expected_ordering)
+
+
 class TestModelVerboseNameMixin:
     """Mixin with function for testing verbose_name of model"""
 
--- post/tests/test_models.py
+++ post/tests/test_models.py
@@ -4,10 +4,19 @@ from django.test import TestCase, tag
 from django.contrib.auth import get_user_model
 
 from post.models import Post, LikeDate, DislikeDate
-from post.tests.mixins import COMMON_MIXINS, TestModelUniqueTogether
+from post.tests.mixins import (
+    COMMON_MIXINS, TestModelUniqueTogether, TestMaxLengthMixin,
+    TestDbIndexMixin, TestAutoNowMixin, TestChoicesMixin, TestBlankMixin,
+    TestDefaultMixin, TestModelOrdering,
+)
 
 
 User = get_user_model()
+
+POST_STATUS = (
+    ("draft", "Draft"),
+    ("publish", "Publish"),
+)
 
 
 @tag('post_model')
@@ -142,3 +151,136 @@ class DislikeDateTests(TestCase, *COMMON_MIXINS, TestModelUniqueTogether):
         """Test verbose_name_plural field for the model"""
 
         super().test_verbose_name_plural_of_model(DislikeDate)
+
+
+@tag('post_model')
+class PostTests(
+    TestCase, *COMMON_MIXINS, TestModelUniqueTogether,
+    TestMaxLengthMixin, TestDbIndexMixin, TestAutoNowMixin,
+    TestChoicesMixin, TestBlankMixin, TestDefaultMixin,
+    TestModelOrdering,):
+    """Tests for Post model"""
+
+    @classmethod
+    def setUpTestData(cls):
+        test_user = User.objects.create(username='test_user',)
+
+        cls.test_post = Post.objects.create(
+            author=test_user,
+            title='Test title',
+            content='Test content',
+        )
+
+        cls.field_and_verbose_name = {
+            'title': 'title',
+            'author': 'author',
+            'content': 'content',
+            'published_time': 'published time',
+            'updated_time': 'updated time',
+            'status': 'status',
+            'likes': 'likes',
+            'dislikes': 'dislikes',
+            'liked_authors': 'liked authors',
+            'disliked_authors': 'disliked authors',
+        }
+
+        cls.field_and_max_length = {
+            'title': 255,
+            'status': 20,
+        }
+
+        cls.field_and_choices = {
+            'status': POST_STATUS,
+        }
+
+        cls.field_and_default = {
+            'status': 'publish',
+            'likes': 0,
+            'dislikes': 0,
+        }
+
+        cls.db_index_fields = (
+            'title', 'content',
+        )
+
+        cls.blank_fields = ('liked_authors', 'disliked_authors',)
+
+        cls.auto_now_add_fields = ('published_time',)
+
+        cls.auto_now_fields = ('updated_time',)
+
+        cls.model_unique_together = ('title', 'author',)
+
+        cls.model_ordering = ('-published_time',)
+
+        cls.model_verbose_name = 'post'
+
+        cls.model_verbose_name_plural = 'posts'
+
+    def test_verbose_name(self):
+        """Test verbose_name field"""
+
+        super().test_verbose_name(Post)
+
+    def test_auto_now_add(self):
+        """Test auto_now_add field"""
+
+        super().test_auto_now_add(Post)
+
+    def test_auto_now(self):
+        """Test auto_now field"""
+
+        super().test_auto_now(Post)
+
+    def test_choices(self):
+        """Test choices field"""
+
+        super().test_choices(Post)
+
+    def test_default(self):
+        """Test default field"""
+
+        super().test_default(Post)
+
+    def test_blank(self):
+        """Test blank field"""
+
+        super().test_blank(Post)
+
+    def test_max_length(self):
+        """Test max_length field"""
+
+        super().test_max_length(Post)
+
+    def test_db_index(self):
+        """Test db_index field"""
+
+        super().test_db_index(Post)
+
+    def test_object_string_display(self):
+        """Test object display"""
+
+        super().test_object_string_display(
+            self.test_post,
+            str(self.test_post.title) + " (" + str(self.test_post.author) + ")"
+        )
+
+    def test_unique_together_of_model(self):
+        """Test unique_together field for the model"""
+
+        super().test_unique_together_of_model(Post)
+
+    def test_ordering_of_model(self):
+        """Test ordering field for the model"""
+
+        super().test_ordering_of_model(Post)
+
+    def test_verbose_name_of_model(self):
+        """Test verbose_name field for the model"""
+
+        super().test_verbose_name_of_model(Post)
+
+    def test_verbose_name_plural_of_model(self):
+        """Test verbose_name_plural field for the model"""
+
+        super().test_verbose_name_plural_of_model(Post)
