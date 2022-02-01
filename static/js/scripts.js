ad6bcdc80eb98e821b1acba5f2b22dfccae01712
OsnovaDT <maestrojane@gmail.com>
2022-02-01 23:43:08 +0300

    Refactroine: delete console.log for dropdown menu scripts

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,10 +1,8 @@
 $('#user_dropdown_menu_arrow').on('click', function(){
     if ($('#user_dropdown_menu').is(':visible')){
-        console.log(1);
         $('#user_dropdown_menu').hide();
     }
     else{
-        console.log(2);
         $('#user_dropdown_menu').show();
     }
 });
